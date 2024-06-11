073a6e0f40c7dfb0bb0b5d80d3e79357a2452b98
Astemir <astemireleev@gmail.com>
2024-06-11 13:43:29 +0300

    Initial Commit

---
+++ FractalBLineMotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FractalBLineMotion
+//
+//  Created by Astemir Eleev on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FractalBLineMotion/FractalBLineMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalBLineMotionApp.swift
+//  FractalBLineMotion
+//
+//  Created by Astemir Eleev on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FractalBLineMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
